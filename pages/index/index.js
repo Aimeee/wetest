e8a6cf43a2d0f127c4dd5b75ea5c60b2ba1ff598
Aimeee <activeyyc@gmail.com>
2021-03-16 17:40:26 +0800

    first

---
+++ pages/index/index.js
@@ -0,0 +1,97 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUseGetUserProfile: true,
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  onGetUserInfo(e) {
+    const userInfo = e.detail.userInfo
+    if (userInfo) {
+      // 1. 小程序通过wx.login()获取code
+      wx.login({
+        success: function(login_res) {
+          //获取用户信息
+          wx.getUserInfo({
+            success: function(info_res) {
+              // 2. 小程序通过wx.request()发送code到开发者服务器
+              console.log('服务器99');
+              wx.request({
+                url: 'http://api.microspicy.com:8077/wx/login',
+                method: 'POST',
+                header: {
+                  'content-type': 'application/x-www-form-urlencoded'
+                },
+                data: {
+                  code: login_res.code, //临时登录凭证
+                  rawData: info_res.rawData, //用户非敏感信息
+                  signature: info_res.signature, //签名
+                  encrypteData: info_res.encryptedData, //用户敏感信息
+                  iv: info_res.iv //解密算法的向量
+                },
+                success: function(res) {
+                  if (res.data.status == 200) {
+                    // 7.小程序存储skey（自定义登录状态）到本地
+                    wx.setStorageSync('userInfo', userInfo);
+                    wx.setStorageSync('skey', res.data.data);
+                  } else{
+                    console.log('服务器异常');
+                  }
+                },
+                fail: function(error) {
+                  //调用服务端登录接口失败
+                  console.log(error);
+                }
+              })
+            }
+          })
+        }
+      })
+      this.setData({
+        hasUserInfo: true,
+        userInfo: userInfo
+      })
+    }
+  }
+})
