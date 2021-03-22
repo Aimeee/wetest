2805c7712e935fb6565b23abcd66a0300e7fdd75
Aimeee <activeyyc@gmail.com>
2021-03-22 18:00:27 +0800

    更改接口
    更改了413新接口profile

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,7 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUse: wx.canIUse('button.open-type.getUserProfile'),
     canIUseGetUserProfile: true,
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
   },
@@ -18,22 +18,47 @@ Page({
     })
   },
   onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
+   
   },
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+      desc: "用用来装逼", // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (inf_res) => {
+        console.log('ok ');
+        console.log(inf_res.userInfo)
+        wx.login({
+          success: function(login_res) {
+          wx.request({
+            url: 'http://127.0.0.1:8077/api/wxuserinfo',
+            method: 'POST',
+            header: {
+              'content-type': 'application/x-www-form-urlencoded'
+            },
+            data: {
+              code: login_res.code, //临时登录凭证
+              userInfo: JSON.stringify(inf_res.userInfo), //用户非敏感信息
+            },
+            success: function(res) {
+              if (res.data.status == 200) {
+                // 7.小程序存储skey（自定义登录状态）到本地
+                wx.setStorageSync('userInfo', userInfo);
+                wx.setStorageSync('skey', res.data.data);
+              } else{
+                console.log('服务器异常');
+              }
+            },
+            fail: function(error) {
+              //调用服务端登录接口失败
+              console.log(error);
+            }
+          })
+        }
+      })
+      },
+      fail: function(error) {
+        //调用服务端登录接口失败
+        console.log(error);
       }
     })
   },
@@ -57,6 +82,7 @@ Page({
             success: function(info_res) {
               // 2. 小程序通过wx.request()发送code到开发者服务器
               console.log('服务器99');
+              console.log('info_res');
               wx.request({
                 url: 'http://api.microspicy.com:8077/wx/login',
                 method: 'POST',
@@ -84,6 +110,10 @@ Page({
                   console.log(error);
                 }
               })
+            },
+            fail: function(error) {
+              //调用服务端登录接口失败
+              console.log(error);
             }
           })
         }
